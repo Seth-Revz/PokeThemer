243dcfa5f648349d58d76568c8bb02c1ac3726e7
Seth-Revz <sethmorris.dev@gmail.com>
2024-02-21 04:56:01 -0500

    partial dump, not everything

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 import xml.etree.ElementTree as ET
 import os
+from glob import glob
 from PIL import Image
 
 
 if __name__ == '__main__':
-    tree = ET.parse('default/cursors.xml')
-    root = tree.getroot()
+    theme = 'default'
 
-    for element in tree.findall('images'):
+    result = [y for x in os.walk(theme) for y in glob(os.path.join(x[0], '*.xml'))]
+    print(*result, sep='\n')
 
-        image_path = element.attrib['file']
-        img = Image.open('default/' + image_path)
+    for file in result:
 
-        for item in element:
-            name = item.attrib['name']
-            if 'xywh' in item.attrib:
-                x, y, w, h = [int(x) for x in item.attrib['xywh'].split(',')]
+        tree = ET.parse(file)
+        root = tree.getroot()
 
-                print(name, x, y, w, h)
-                cut = img.crop((x, y, x+w, y+h))
-                cut.save(f'{name}.png')
+        for element in tree.findall('images'):
+
+            image_path = element.attrib['file']
+            if '.png' not in image_path:
+                continue
+
+            output_folder_name = 'output/' + image_path.split('/')[-1].rstrip('.png')
+
+            img = Image.open(f'{theme}/{image_path}')
+
+            for item in element:
+                name = item.attrib['name']
+                if 'xywh' in item.attrib and item.attrib['xywh'] != '*':
+                    x, y, w, h = [int(x) for x in item.attrib['xywh'].split(',')]
+
+                    print(name, x, y, w, h)
+
+                    if w < 0:
+                        continue 
+                    
+                    cut = img.crop((x, y, x+w, y+h))
+                    if not os.path.exists(output_folder_name):
+                        os.makedirs(output_folder_name)
+                    cut.save(f'{output_folder_name}/{name}.png')
